2de8b310fcf39b270d0d65dfb18c26ed8b2b621b
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 13:49:37 -0700

    uploaded KanoTerminator.py

---
+++ KanoTerminator.py
@@ -0,0 +1,12 @@
+# Kano or Terminator
+# By Jean-Jacques F. Reibel
+# I will not be held responsible for:
+# any shenanigans
+import os
+# ಠ_ಠ
+# ¯¯\_(ツ)_/¯¯
+# (╭ರ_•́)
+
+os.system("printf '\e[0;35;1;1m        (╭ರ_'")
+os.system("printf '\e[0;31;1;5m°'")
+os.system("printf '\e[0;35;1;1m)\n'")
